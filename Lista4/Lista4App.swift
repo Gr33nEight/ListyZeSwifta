8a5564b43e8e2dc4602d59190fcd13bfbf8694f2
Gr33nEight <natanael.jop.app@gmail.com>
2025-02-24 15:55:43 +0100

    Initial Commit

---
+++ Lista4/Lista4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lista4App.swift
+//  Lista4
+//
+//  Created by Natanael Jop on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lista4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
