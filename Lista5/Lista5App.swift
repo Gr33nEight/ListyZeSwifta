28835cec568f37d3526c7329c7cf5fce5aa82b3a
Gr33nEight <natanael.jop.app@gmail.com>
2025-02-24 18:42:58 +0100

    Initial Commit

---
+++ Lista5/Lista5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lista5App.swift
+//  Lista5
+//
+//  Created by Natanael Jop on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lista5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
