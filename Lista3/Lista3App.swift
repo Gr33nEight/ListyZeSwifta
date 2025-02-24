fd9eb1ab1538cb8491bcd57ded9f4017956dafb5
Gr33nEight <natanael.jop.app@gmail.com>
2025-02-24 15:39:39 +0100

    Initial Commit

---
+++ Lista3/Lista3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lista3App.swift
+//  Lista3
+//
+//  Created by Natanael Jop on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lista3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
