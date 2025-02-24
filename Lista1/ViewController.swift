d22adcdb3483ce6edfc55730e3bfd6a21aadd7a9
Gr33nEight <natanael.jop.app@gmail.com>
2025-02-24 14:42:26 +0100

    Initial Commit

---
+++ Lista1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lista1
+//
+//  Created by Natanael Jop on 24/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
