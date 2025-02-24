de35b2d2a2075ab23df21263cd2ed91b8075166d
Gr33nEight <natanael.jop.app@gmail.com>
2025-02-24 19:03:37 +0100

    Initial Commit

---
+++ Lista6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lista6
+//
+//  Created by Natanael Jop on 24/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lista6/Lista6App.swift
@@ -0,0 +1,17 @@
+//
+//  Lista6App.swift
+//  Lista6
+//
+//  Created by Natanael Jop on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lista6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
